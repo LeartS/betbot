97f6f0ea9b510965073861bf7607297e18cb2e66
Leonardo Donelli <learts92@gmail.com>
2014-12-14 01:40:46 +0100

    Slight refactor, working BWin site, add cipro.

--- betbot.py
+++ betbot.py
@@ -20,17 +20,9 @@ class Site(object):
     url = ''
     url_params = {}
 
-    def parse_html_for_quotes(self, parsed_html):
-        if DEBUG: print('Parsing HTML for generic site')
+    def parse_response(self, reponse):
+        if DEBUG: print('Parsing reponse for generic site')
         pass
-
-    def get_html_tree(self, url):
-        r = requests.get(url)
-        html_string = BytesIO(r.text.encode('UTF-8'))
-        parser = etree.HTMLParser(remove_blank_text=True, remove_comments=True,
-                                  recover=True)
-        html = etree.parse(html_string, parser)
-        return html
 
     def get_league_quotes(self, sport, location):
         if DEBUG:
@@ -42,22 +34,43 @@ class Site(object):
         except KeyError:
             # This site does not have this league, so no quotes
             return []
-        html = self.get_html_tree(
-            self.url.format(sport=sport_id, location=location_id)
-        )
-        return self.parse_html_for_quotes(html)
+        response = requests.get(self.url.format(sport=sport_id,
+                                                location=location_id))
+        return self.parse_response(response)
 
 
 class BWin(Site):
-    url = 'https://m.bwin.it/#/?sport={sport}&location={location}'
+    url = ('https://it-it.mobileapi.bwinlabs.com/api/iphone/v2/events/search?'
+           'sportid={sport}&regionid={location}&fields=details.('
+           'short_name%2Cstarts_at%2Cleague%2Cparticipants)%2Cscore_board'
+           '%2Ctype%2Ceventids%2Cgames%5B0%5D.(id%2Cname%2C'
+           'results.(name%2Codds))&sort=live%20desc%2Cstartsat%20asc%2Csportid'
+           '%20asc%2Ceventname%20asc&content=list&page_number=1&page_size=40'
+           '&partnerid=mobit2013')
+    # url = 'https://m.bwin.it/#/?sport={sport}&location={location}'
     url_params = {
         'soccer': {
             'id': 4,
             'locations': {
                 'poland': 22,
+                'cipro': 58,
             },
         }
     }
+
+    def parse_response(self, response):
+        if DEBUG: print('Parsing quotes as JSON for BWin')
+        data = response.json()
+        matches_quotes = {}
+        for event in data['response']['items']['events']:
+            game = [g for g in event['non_live']['games'] if g['name'] == '1X2']
+            try:
+                game = game[0]
+            except IndexError:
+                continue
+            matches_quotes[event['details']['short_name']] = tuple(
+                result['odds'] for result in game['results'])
+        return matches_quotes
 
 
 class Sisal(Site):
@@ -67,15 +80,20 @@ class Sisal(Site):
             'id': 1,
             'locations': {
                 'poland': 183,
+                'cipro': 290,
             },
         }
     }
 
-    def parse_html_for_quotes(self, html_tree):
-        if DEBUG: print('Parsing HTML for Sisal')
-        match_nodes = html_tree.getroot().xpath(
+    def parse_response(self, response):
+        if DEBUG: print('Parsing response as HTML tree for Sisal')
+        data = BytesIO(response.text.encode('utf-8'))
+        parser = etree.HTMLParser(remove_blank_text=True, remove_comments=True,
+                                  recover=True)
+        html = etree.parse(data, parser)
+        match_nodes = html.getroot().xpath(
             '//table[@class="event-header"]//span[@class="fs15px b"]')
-        quote_nodes = html_tree.getroot().xpath(
+        quote_nodes = html.getroot().xpath(
             '//span[@class="odds-convert"]')
         matches_quotes = {}
         for match, quotes in zip(
@@ -87,5 +105,5 @@ class Sisal(Site):
 
 
 if __name__ == '__main__':
-    sites_manager = SitesManager([Sisal()])
-    sites_manager.check_for_sure_bets('soccer', 'poland')
+    sites_manager = SitesManager([Sisal(), BWin()])
+    sites_manager.check_for_sure_bets('soccer', 'cipro')
